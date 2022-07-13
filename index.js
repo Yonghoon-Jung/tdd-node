3e5d496f86c8ddf4007f8deb2ae2852c71b0200c
Yonghoon <yonghoonj98@gmail.com>
2022-07-13 10:59:20 +0900

    express Hello world 구현

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
 
-function commonmw(req, res, next) {
-  console.log('commonmw');
-  next(new Error('error'));
-}
-
-function errormw(err, req, res, next) {
-  console.log(err.message);
-  // 에러를 처리하거나
-  // 에러를 넘기거나
-  next();
-}
-
-app.use(commonmw);
-app.use(errormw);
-
-app.listen(3000, () => {
-  console.log('Server is running');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+//-------------------------------
+// const express = require('express');
+// const app = express();
+
+// function commonmw(req, res, next) {
+//   console.log('commonmw');
+//   next(new Error('error'));
+// }
+
+// function errormw(err, req, res, next) {
+//   console.log(err.message);
+//   // 에러를 처리하거나
+//   // 에러를 넘기거나
+//   next();
+// }
+
+// app.use(commonmw);
+// app.use(errormw);
+
+// app.listen(3000, () => {
+//   console.log('Server is running');
+// });
 
 //-------------------------------
 // const morgan = require('morgan');
