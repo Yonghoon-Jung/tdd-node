7763970de56d6f271b12515a68ec60bc3bf3de23
Yonghoon <yonghoonj98@gmail.com>
2022-07-13 14:30:15 +0900

    서버 구동 로직 분리

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const user = require('./api/user');
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', user);
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 
 module.exports = app;
 
