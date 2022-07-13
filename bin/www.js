09fc37d8d961addd709722ded90aa384b6cd1a13
Yonghoon <yonghoonj98@gmail.com>
2022-07-13 14:48:47 +0900

    DB 동기화 완료

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,9 @@
 const app = require('../index');
+const syncDb = require('./sync-db');
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+syncDb().then((result) => {
+  console.log('sync database');
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
 });
