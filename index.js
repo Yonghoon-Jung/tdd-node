9c0ab4a33012dbf08c1e01c42252ddd9c7c543f9
Yonghoon <yonghoonj98@gmail.com>
2022-07-13 11:39:30 +0900

    슈퍼테스트 적용

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+module.exports = app;
+
 //-------------------------------
 // const express = require('express');
 // const app = express();
