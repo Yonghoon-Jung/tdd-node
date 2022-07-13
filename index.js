4991103d995a3c6c7abdf8ba56146ef47b2d31dd
Yonghoon <yonghoonj98@gmail.com>
2022-07-13 11:08:05 +0900

    get, post, put, delete 공부

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/users', function (req, res) {
+  res.send('User List!');
+});
+
+app.post('/users', function (req, res) {
+  res.send('Create User!');
 });
 
 app.listen(3000, function () {
